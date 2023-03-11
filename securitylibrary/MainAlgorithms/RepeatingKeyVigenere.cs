3c8b408b847c80c63ca76a9d55e8d72234faa5c6
Ahmed Kandeel <1kandeel1@gmail.com>
2023-03-12 00:45:46 +0200

    Vigenere (rep+auto) without Analyse fun

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,6 +8,7 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
+        int mod(int x, int m){ return (x % m + m) % m; }
         public string Analyse(string plainText, string cipherText)
         {
             throw new NotImplementedException();
@@ -15,12 +16,23 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            while (key.Length < cipherText.Length)
+                key += key;
+            string plainText = "";
+            for (int i = 0; i < cipherText.Length; i++)
+                plainText += (char)( 'a' + mod((cipherText[i] - key[i] ) , 26));
+            return plainText;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            while (key.Length < plainText.Length)
+                key += key;
+            string cipherText="";
+            for(int i = 0; i < plainText.Length; i++)
+                cipherText += (char)('a'+(( plainText[i]-'a' + key[i]-'a' )% 26));
+            return cipherText;
         }
     }
 }
