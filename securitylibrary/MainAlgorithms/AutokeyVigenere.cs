0ff9a3444f80934c474874d486fa3fe72e9d1815
Omar Tarek <omar.tarek615243@gmail.com>
2023-03-23 16:10:43 +0200

    analysis fnc

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -11,14 +11,31 @@ namespace SecurityLibrary
         int mod(int x, int m) { return (x % m + m) % m; }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+
+            string keystream = "";
+            for (int i = 0; i < cipherText.Length; i++)
+                keystream += (char)('a' + mod((cipherText[i] - plainText[i]), 26));
+            string key = plainText.Substring(0, 6);
+            for (int i = 3; i < keystream.Length; i++)
+            {
+                if (key == keystream.Substring(i, 6))
+                {
+                    keystream = keystream.Remove(i);
+                }
+
+            }
+
+            return keystream;
         }
 
         public string Decrypt(string cipherText, string key)
         {
             cipherText = cipherText.ToLower();
             string plainText = "";
-            for (int i = 0; i < cipherText.Length; i++) { 
+            for (int i = 0; i < cipherText.Length; i++)
+            {
                 char tmp = (char)('a' + mod((cipherText[i] - key[i]), 26));
                 plainText += tmp;
                 key += tmp;
--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -8,10 +8,26 @@ namespace SecurityLibrary
 {
     public class RepeatingkeyVigenere : ICryptographicTechnique<string, string>
     {
-        int mod(int x, int m){ return (x % m + m) % m; }
+        int mod(int x, int m) { return (x % m + m) % m; }
         public string Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            plainText = plainText.ToLower();
+
+            string keystream = "";
+            for (int i = 0; i < cipherText.Length; i++)
+                keystream += (char)('a' + mod((cipherText[i] - plainText[i]), 26));
+            string key = keystream.Substring(0, 3);
+            for (int i = 3; i < keystream.Length; i++)
+            {
+                if (key == keystream.Substring(i, 3))
+                {
+                    keystream = keystream.Remove(i);
+                }
+
+            }
+
+            return keystream;
         }
 
         public string Decrypt(string cipherText, string key)
@@ -21,7 +37,7 @@ namespace SecurityLibrary
                 key += key;
             string plainText = "";
             for (int i = 0; i < cipherText.Length; i++)
-                plainText += (char)( 'a' + mod((cipherText[i] - key[i] ) , 26));
+                plainText += (char)('a' + mod((cipherText[i] - key[i]), 26));
             return plainText;
         }
 
@@ -29,9 +45,9 @@ namespace SecurityLibrary
         {
             while (key.Length < plainText.Length)
                 key += key;
-            string cipherText="";
-            for(int i = 0; i < plainText.Length; i++)
-                cipherText += (char)('a'+(( plainText[i]-'a' + key[i]-'a' )% 26));
+            string cipherText = "";
+            for (int i = 0; i < plainText.Length; i++)
+                cipherText += (char)('a' + ((plainText[i] - 'a' + key[i] - 'a') % 26));
             return cipherText;
         }
     }
