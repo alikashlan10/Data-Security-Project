1162a6cab5a35da16405fb6ed5c61f26aad070b8
Ahmed Kandeel <1kandeel1@gmail.com>
2023-05-11 22:31:16 +0300

    a

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SecurityLibrary.RSA
 {
@@ -10,6 +6,7 @@ namespace SecurityLibrary.RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
+
             throw new NotImplementedException();
         }
 
