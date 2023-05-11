cc9d92c0ab185472ddb473dc50f7ce209bf33141
Ahmed Kandeel <1kandeel1@gmail.com>
2023-05-11 22:34:06 +0300

    LAST

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SecurityLibrary.DiffieHellman
 {
@@ -10,7 +6,53 @@ namespace SecurityLibrary.DiffieHellman
     {
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            List<int> Key = new List<int>();
+            int K1, K2;
+
+            int Ya = ModPower(alpha, xa, q);
+            int Yb = ModPower(alpha, xb, q);
+            K1 = ModPower(Ya, xb, q);
+            Key.Add(K1);
+            K2 = ModPower(Yb, xa, q);
+            Key.Add(K2);
+            return Key;
+        }
+
+        //bgm3 mod
+        public static int Mod(int a, int b, int mod)
+        {
+
+            int r = (a * b) % mod;
+            int x;
+            if (r < 0)
+            {
+                x = r + mod;
+            }
+            else
+            {
+                x = r;
+            }
+            return x;
+        }
+
+        //b3ml divide le large power
+        public static int ModPower(int Base, int power, int mod)
+        {
+            int result = 1;
+
+            while (power > 0)
+            {
+                if (power % 2 == 1)
+                {
+                    result = Mod(result, Base, mod);
+                }
+
+                Base = Mod(Base, Base, mod);
+                power /= 2;
+            }
+
+            return result;
         }
     }
 }
--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -1,18 +1,74 @@
-﻿using System;
-
-namespace SecurityLibrary.RSA
+﻿namespace SecurityLibrary.RSA
 {
     public class RSA
     {
         public int Encrypt(int p, int q, int M, int e)
         {
+            int n = p * q;
+            //Calculate C = M^e mod n when M is too large
+            int c = largepower(M, e, n);
+            return c;
 
-            throw new NotImplementedException();
         }
 
         public int Decrypt(int p, int q, int C, int e)
         {
-            throw new NotImplementedException();
+            int d;
+            int n = p * q;
+            int Euler = (p - 1) * (q - 1);
+            //d= einverse mod euler 
+            d = mulinverse(e, Euler);
+            //Calculate M=C^d mod n when C is too large
+            int M = largepower(C, d, n);
+            return M;
         }
+
+        //bt7sb el mod lama ykon el rkm kber
+        public int largepower(int M, int e, int n)
+        {
+            long result = 1;
+            long baseValue = M % n;
+            while (e > 0)
+            {
+                if ((e & 1) == 1)
+                {
+                    result = (result * baseValue) % n;
+                }
+                baseValue = (baseValue * baseValue) % n;
+                e >>= 1;
+            }
+            return (int)result;
+        }
+
+        //bt7sb el multiplicative inverse 3shan ngeb el d fl decrypt
+        public int mulinverse(int a, int m)
+        {
+            int m0 = m;
+            int y = 0, x = 1;
+
+            if (m == 1)
+                return 0;
+
+            while (a > 1)
+            {
+
+                int q = a / m;
+                int t = m;
+
+                m = a % m;
+                a = t;
+                t = y;
+
+                y = x - q * y;
+                x = t;
+            }
+
+            if (x < 0)
+                x += m0;
+
+            return x;
+        }
+
+
     }
 }
